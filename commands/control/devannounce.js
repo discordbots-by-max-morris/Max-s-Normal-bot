fb098bb79764f24655b98a5678681a73e879a7d7
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-04-13 12:42:49 -0400

    Update devannounce.js

--- commands/control/devannounce.js
+++ commands/control/devannounce.js
@@ -1,1 +1,41 @@
+const { Command } = require('discord.js-commando');
+const { oneLine } = require('common-tags');
+const { RichEmbed } = require('discord.js');
 
+module.exports = class AnnounceCommand extends Command {
+  constructor(bot) {
+    super(bot, {
+      name: 'announce',
+      aliases: ['update', 'lann', 'sendupdate'],
+      group: 'control',
+      memberName: 'announce',
+      description: 'Sends an announcemnt to #announcements in SERVER',
+      details: oneLine`
+		    This command sends an announcemnt to #announcements in SERVER
+            Usage is restricted to bot owners.
+			`,
+      examples: ['announce'],
+      args: [{
+        key: 'toAnn',
+        label: 'announce',
+        prompt: 'What would you like to announce?',
+        type: 'string',
+        infinite: false
+      }],
+      guarded: true,
+      ownerOnly: true
+    });
+  }
+
+  async run(message, args) {
+    const annChan = this.client.channels.get('566664566298378240');
+    const embed = new RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setDescription(args.toAnn)
+      .setColor('0xFF0000')
+      .setTimestamp();
+    annChan.send('<@&556361477653659659>', { embed }).then(() => {
+      message.reply('Announcement sent!');
+    });
+  }
+};
