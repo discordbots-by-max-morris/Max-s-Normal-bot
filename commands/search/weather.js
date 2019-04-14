f3551962d1a7d0768e4200b20d3cc63a572afb4b
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-04-14 16:34:38 -0400

    Update weather.js

--- commands/search/weather.js
+++ commands/search/weather.js
@@ -1,65 +1,47 @@
 const { Command } = require('discord.js-commando');
 const { RichEmbed } = require('discord.js');
-const request = require('superagent');
+const weather = require('weather-js');
 
 module.exports = class WeatherCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'weather',
-            group: 'search',
+            aliases: ['temp'],
+            group: 'utilisation',
             memberName: 'weather',
-            description: 'Gets weather information for a specified location.',
-            args: [
+            description: 'Search the Weather for your location',
+            examples: ['{prifix}weather [location]'],
+            guildOnly: true,
+            args:[
                 {
-                    key: 'query',
-                    prompt: 'What location would you like to get the current weather for?',
-                    type: 'string'
+                    key: 'location',
+                    prompt: 'what location to check for',
+                    type: 'string',
+                    default: ''
                 }
             ]
         });
     }
-
-    async run(msg, args) {
-        if (msg.channel.type !== 'dm')
-            if (!msg.channel.permissionsFor(this.client.user).has('EMBED_LINKS'))
-                return msg.say('This Command requires the `Embed Links` Permission.');
-        const { query } = args;
-        try {
-            const { body } = await request
-                .get(`https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where u=\'f\' AND woeid in (select woeid from geo.places(1) where text="${query}")&format=json`);
-            if (!body.query.count) throw new Error('Location Not Found.');
+    async run(message, args){
+        weather.find({search: args.location, degreeType: 'C'}, function(err, result) {
+            if (err) message.channel.send(err);
+            if (result === undefined || result.length === 0) {
+                message.channel.send('**Please enter a location!**')
+                return;
+            }
+            var current = result[0].current;
+            var location = result[0].location;
             const embed = new RichEmbed()
-                .setColor(0x0000FF)
-                .setAuthor(body.query.results.channel.title, 'https://i.imgur.com/2MT0ViC.png')
-                .setURL(body.query.results.channel.link)
-                .setTimestamp()
-                .addField('City',
-                    body.query.results.channel.location.city, true)
-                .addField('Country',
-                    body.query.results.channel.location.country, true)
-                .addField('Region',
-                    body.query.results.channel.location.region, true)
-                .addField('Condition',
-                    body.query.results.channel.item.condition.text, true)
-                .addField('Temperature',
-                    `${body.query.results.channel.item.condition.temp}°F`, true)
-                .addField('Humidity',
-                    body.query.results.channel.atmosphere.humidity, true)
-                .addField('Pressure',
-                    body.query.results.channel.atmosphere.pressure, true)
-                .addField('Rising',
-                    body.query.results.channel.atmosphere.rising, true)
-                .addField('Visibility',
-                    body.query.results.channel.atmosphere.visibility, true)
-                .addField('Wind Chill',
-                    body.query.results.channel.wind.chill, true)
-                .addField('Wind Direction',
-                    body.query.results.channel.wind.direction, true)
-                .addField('Wind Speed',
-                    body.query.results.channel.wind.speed, true);
-            return msg.embed(embed);
-        } catch (err) {
-            return msg.say(err);
-        }
-    }
+                .setDescription(`**${current.skytext}**`)
+                .setAuthor(`Weather for ${current.observationpoint}`)
+                .setThumbnail(current.imageUrl)
+                .setColor('RANDOM')
+                .addField('Timezone',`UTC${location.timezone}`, true)
+                .addField('Temperature',`${current.temperature}°C`, true)
+                .addField('Feels Like', `${current.feelslike}°C`, true)
+                .addField('Winds',current.winddisplay, true)
+                .addField('Humidity', `${current.humidity}%`, true)
+            return message.channel.send(embed);
+        })
+      }
 };
